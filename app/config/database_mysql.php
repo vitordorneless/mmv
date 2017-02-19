a51384d6986442fb11681b60542d489a7ef10ade
vitordorneless <vitordorneles@hotmail.com>
2017-02-18 22:24:50 -0200

    mudança de css
    para um melhor, com mais tools

---
+++ app/config/database_mysql.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+class Database {
+
+    private static $dbName = "mmv";
+    private static $dbHost = "localhost";
+    private static $dbUsername = "root";    
+    private static $dbUserPassword = "";
+    private static $cont = null;
+
+    public function __construct() {
+        die('Não rolou');
+    }
+
+    public static function connect() {
+        if (null == self::$cont) {
+            try {
+                self::$cont = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect() {
+        self::$cont = null;
+    }
+}
